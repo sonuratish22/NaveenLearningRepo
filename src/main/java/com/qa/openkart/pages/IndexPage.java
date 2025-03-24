5010f9b4b743ef12ed6a1308751fd8072b11c33e
Sonu Kumar <sonuratish22@gmail.com>
2025-03-24 23:59:29 +0530

    Added Index Page for Home Link

---
+++ src/main/java/com/qa/openkart/pages/IndexPage.java
@@ -0,0 +1,11 @@
+package com.qa.openkart.pages;
+
+public class IndexPage {
+    public IndexPage(){
+        System.out.println("This is my index constructor");
+    }
+
+    public void indexHomePage(){
+        System.out.println("This is home page link");
+    }
+}
